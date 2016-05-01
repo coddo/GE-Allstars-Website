7634e5e28b8e6fbf8a2b1f741b51c2f8bd19c2f7
coddo <codobanclaudiu@gmail.com>
2016-05-01 17:27:01 +0300

    Updated the main snippet and also removed some files from tracking

--- app/components/MainPage.jsx
+++ app/components/MainPage.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {X: 10, Y: 20, Z: 30};
+        
+        this.changeState = this.changeState.bind(this);
+    }
+    
+    changeState() {
+        this.setState({Y:10000});
+    }
+    
     render() {
-        return <h1>HELLO WORLD!</h1>
+        return (
+            <div id="MainPage">
+                <h1>HELLO WORLD!</h1>
+                <h1>{this.state.X}</h1>
+                <h1>{this.state.Y}</h1>
+                <h1>{this.state.Z}</h1>
+                <br/><br/>
+                <button onClick={this.changeState}>CHANGE THE STATE</button>
+            </div>
+        );
     }
 }
+
 
 ReactDOM.render(<Main/>, document.getElementById("app"))
